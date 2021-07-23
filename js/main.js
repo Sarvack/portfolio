635d976e24115842a67a83ef14424f586779ebc5
Sarvack <sarvajanaprya@gmail.com>
2021-07-23 13:15:25 +0800

    minutfourtyfive

--- js/main.js
+++ js/main.js
@@ -19,3 +19,37 @@
             }
     })
 })();
+
+
+// portfolio filter and pop up 
+
+(() =>{
+        const filterContainer = document.querySelector(".portfolio-filter"),
+        portfolioItemsContainer = document.querySelector(".portfolio-items"),
+        portfolioItems = document.querySelectorAll('.portfolio-item'),
+        popup = document.querySelector(".portfolio-popup"),
+        prevBtn = document.querySelector(".pp-prev"),
+        nextBtn = document.querySelector(".pp-next"),
+        close = popup.querySelectorAll(".pp-close"),
+        projectDetailsContainer = popup.querySelector(".pp-details"),
+        projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+
+        let itemIndex, slideIndex, screenshots;
+
+        // filter portfolio
+
+        filterContainer.addEventListener("click", (event)=>{
+            if (event.target.classList.contains("filter-item") && 
+            !event.target.classList.contains("active")){
+                filterContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+                // activate new 'filter item'
+                event.target.classList.add("active","outer-shadow");
+                const target = event.target.getAttribute("data-target");
+                portfolioItems.forEach((item) =>{
+                    if(target === item.getAttribute("date-category")){
+                        
+                    }
+                })
+            }
+        })
+})();
