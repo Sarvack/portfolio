1920197215334ad9909bfb587c2458e581803ba2
Sarvack <sarvajanaprya@gmail.com>
2021-09-07 14:18:17 +0800

    testimonial

--- js/main.js
+++ js/main.js
@@ -159,3 +159,67 @@ function bodyScrollingToggle(){
     }
 
 })();
+
+// --------------------testimonial slider ----------------
+(() =>{
+    const sliderContainer = document.querySelector(".testi-slider-container"),
+    slides = sliderContainer.querySelectorAll(".testi-item"),
+    slideWidth = sliderContainer.offsetWidth,
+    prevBtn = document.querySelector(".testi-slider-nav .prev"),
+    nextBtn = document.querySelector(".testi-slider.nav .next");
+
+    let slideIndex = 0;
+
+    slides.forEach((slide) =>{
+        slide.style.width = slideWidth + "px";
+    })
+
+    sliderContainer.style.width = slideWidth + slides.length + "px";
+})();
+
+// ----------testimonial slider ----------------------
+(() =>{
+    const sliderContainer = document.querySelector(".testi-slider-container"),
+    slides = sliderContainer.querySelectorAll(".testi-item"),
+    slideWidth = sliderContainer.offsetWidth;
+    prevBtn = document.querySelector(".testi-slider-nav .prev"),
+    nextBtn = document.querySelector(".testi-slider-nav .next");
+    activeSlide = sliderContainer.querySelector(".testi-item.active");
+
+    let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+    console.log(slideIndex);
+
+    slides.forEach((slide) =>{
+        slide.style.width = slideWidth + "px";
+    })
+
+    sliderContainer.style.width = slideWidth * slides.length + "px";
+
+    nextBtn.addEventListener("click", () => {
+        if(slideIndex === slides.length-1){
+            slideIndex = 0;
+        }
+        else{
+            slideIndex++;
+        }
+        slider();
+    })
+
+    prevBtn.addEventListener("click", ()=>{
+        if(slideIndex === 0){
+            slideIndex = slides.length-1;
+        }
+        else{
+            slideIndex--;
+        }
+        slider();
+    })
+
+    function slider() {
+        sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+        slides[slideIndex].classList.add("active");
+        sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px";
+    }
+
+    slider();
+})();
